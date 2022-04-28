d3a8eef5ac395f512b2b52271c98b99f3e6be76e
verissimo francis <verissimotoyin50@gmail.com>
2022-04-28 18:46:51 +0100

    Update to teacher test

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -5,4 +5,9 @@ describe Teacher do
     teacher = Teacher.new(66, 'JavaScript', 'Barry')
     expect(teacher).to have_attributes(age: 66, specialization: 'JavaScript', name: 'Barry')
   end
+
+  it 'Teacer can use services' do
+    teacher = Teacher.new(66, name: 'Barry', specialization: 'JavaScript')
+    expect(teacher.can_use_services?).to be(true)
+  end
 end
