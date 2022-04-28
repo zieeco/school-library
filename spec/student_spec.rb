08a9d8d66d86867c4525dccb7fd1c96e05741b55
zieeco <zieecosam@gmail.com>
2022-04-28 19:20:19 +0100

    Update student and classroom test files

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,5 @@
 require './student'
+require './classroom'
 
 describe Student do
   context 'Student class' do
@@ -8,6 +9,24 @@ describe Student do
 
     it 'should return all the student attribute' do
       expect(@student).to have_attributes(age: 100, classroom: 'classroom', name: 'Verissimo', parent_permission: true)
+    end
+
+    context 'Student subject ' do
+      it 'the student should have an Math classroom' do
+        std_sub = Student.new(200, 'Math', 'Verissimo', true)
+        math = Classroom.new('Math')
+        std_sub.classroom = math
+        expect(std_sub.classroom.label).to eq 'Math'
+      end
+    end
+
+    context 'Student classroom count' do
+      it 'should return the count of a subject' do
+        std_sub = Student.new(200, 'English', 'Verissimo', true)
+        math = Classroom.new('English')
+        std_sub.classroom = math
+        expect(math.students.count).to eq 1
+      end
     end
   end
 end
